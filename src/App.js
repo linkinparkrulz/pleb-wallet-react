80c954bd16336abdcca76d91b0d0f727539f6f46
linkinparkrulz <linkinparkrulz@protonmail.com>
2022-12-16 06:45:22 -0600

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from "axios"
+
 
 function App() {
+  // useState lets us store/update/pass data from inside of this component and also refresh the component when the data changes
+  // Though this data will be lost on a refresh since we dont have a database
+  const [price, setPrice] = useState(0);
+
+  const getPrice = () => {
+    // Axios is a library that makes it easy to make http requests
+    // After we make a request, we can use the .then() method to handle the response asychronously
+    // This is an alternative to using async/await
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      // .then is a promise that will run when the API call is successful
+      .then((res) => {
+        setPrice(res.data.data.amount);
+      })
+      // .catch is a promise that will run if the API call fails
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bitcoin Price</h1>
+      <h3>${price}</h3>
     </div>
   );
 }
